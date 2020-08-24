c9c95a64d0d7de94221ee1ebbf64f3760f19fc70
Ranveer Oshan <ranveermedran@gmail.com>
2020-08-24 19:55:47 +0530

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,74 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const getNotes = () => {
+    return 'Your notes...'
+}
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNote = notes.find((note) => note.title === title)
+
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
+    } else {
+        console.log(chalk.red.inverse('Note title taken!'))
+    }
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter((note) => note.title !== title)
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
+    }    
+}
+
+const readNotes = (title) => {
+    const notes = loadNotes()
+    const noteTitle = notes.find((note) => note.title === title)
+     if(noteTitle) {
+         console.log(chalk.inverse(noteTitle.title))
+         console.log(noteTitle.body)
+     } else {
+         console.log(chalk.red.inverse('Note title not found!'))
+     }
+
+}
+
+const listNotes = () => {
+    const notes = loadNotes()
+    notes.forEach((cur) => console.log(chalk.cyanBright(cur.title, cur.body)))
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes ,
+    readNotes: readNotes
+}
